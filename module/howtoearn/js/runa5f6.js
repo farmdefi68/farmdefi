cefd3b6a176cbe038a2ca1a1d73380c332d737b7
farmdefi68 <kietboi52@gmail.com>
2022-03-03 19:02:51 +0700

    haha

---
+++ module/howtoearn/js/runa5f6.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function () {
+	setTimeout(function(){
+		$(".video-auto-play").html('<video class="b-rol b-12 i-amphtml-pool-media i-amphtml-pool-video i-amphtml-fill-content i-amphtml-replaced-content" id="i-amphtml-pool-media-4" playsinline="" webkit-playsinline="" loop="true"  poster=" " muted="true" autoplay="true"><source src="https://farmme.io/module/howtoearn/images/gameplay.mp4"></video>')
+	},2000);
+});
